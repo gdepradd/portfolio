d564f08d020f517f22f71536231f476f4cea208d
gdepradd <gedenanda4444@gmail.com>
2021-12-31 22:09:40 +0700

    Bismillah web pertama

---
+++ JavaScript/index.js
@@ -0,0 +1,51 @@
+function question(){
+    var a = "aaaa";
+    var b = "what?";
+    document.getElementById("aaaa").classList.toggle("hidden");
+    if(document.getElementById("aaaa").classList.contains("hidden")){
+    document.getElementById("terserah").innerHTML=b;
+    }
+    else{
+        document.getElementById("terserah").innerHTML=a;
+    }
+    
+}
+const txtElement = ['Assalamualaikum everyone, welcome to my portfolio'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '' ;
+
+// function typing(){
+
+// 	if (count == txtElement.length) {
+// 		count = 0;
+// 	}
+
+// 	currentTxt = txtElement[count];
+
+// 	words = currentTxt.slice(0, ++txtIndex);
+// 	document.querySelector('.efect').textContent = words;
+
+// 	if (words.length == currentTxt.length) {
+// 		count++;
+// 		txtIndex = 0;
+// 	}
+
+// 	setTimeout(typing, 100);
+// };
+function authenticate(){
+	const username = document.getElementById("uname");
+	const password = document.getElementById("pass");
+	
+	if (username.value==="Gede Pradnyananda" && password.value==="gede123"){
+		window.location= "something.html";
+		
+	}
+	else{
+		alert("KAMU BUKAN GEDE!!!");
+	}
+}
+function toggleSidebar(){
+    document.querySelector('.sidebar').classList.toggle('hidden')
+}
